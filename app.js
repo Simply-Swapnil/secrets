5a68c1e1eed03508bddad13356d2ee66291ada7a
Swapnil <swapnilmhoprekar18139@gmail.com>
2022-07-09 13:16:43 +0530

    Level 3: Hashing

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -18,9 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-//The .env file contains SECRET="The key to all"
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -39,7 +35,7 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -54,7 +50,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res){
     username = req.body.username;
-    password = req.body.password;
+    password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, user){
         if(err){

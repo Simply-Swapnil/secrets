d9e444d87dab3ddc41edc48feb5a43c5531c4b98
Swapnil <swapnilmhoprekar18139@gmail.com>
2022-07-11 15:28:30 +0530

    Level 6: Oauth

--- app.js
+++ app.js
@@ -1,114 +1,154 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 const app = express();
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Using express-session module
-app.use(session({
-    secret: 'The key to all', 
+app.use(
+  session({
+    secret: "The key to all",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 //Using passport module
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = new mongoose.Schema({ 
-    email: String,
-    password: String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
 //This plugin uses salting and hashing automatically.
+userSchema.plugin(findOrCreate);      //TENDENCY TO MISS THIS
 
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());  //create session cookie and enables user access without reapeted authentication
-passport.deserializeUser(User.deserializeUser());   //destroy session cookie
+//create session cookie and enables user access without reapeted authentication
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
 
-app.get('/', function(req, res) {
-    res.render('home');
-})
+//destroy session cookie
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
-app.get('/login', function(req, res) {
-    res.render('login');
-})
-
-app.get('/register', function(req, res) {
-    res.render('register');
-})
-
-app.get('/secrets', function(req, res) {
-    if(req.isAuthenticated()){
-        res.render('secrets');
-    }else{
-        res.redirect('/login');
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile); //For testing
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
     }
-})
+  )
+);
 
-app.get('/logout', function(req, res){
-    //callback is made necessary now else only req.logout() was sufficient
-    req.logout(function(err){
-        if(err){ return next(err);}
-        res.redirect('/');
-    });
-    
-})
+app.get("/auth/google", passport.authenticate("google", { scope: "profile" }));
 
-app.post('/register', function(req, res) {
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  }
+);
 
-    const username = req.body.username;
-    const password = req.body.password;
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-    //Before it was needed to create new user model and then save it. But the modules enable new methods.
-    User.register({username: username}, password, function(err, user) {
-        if (err) {
-            console.log(err);
-            res.redirect('/register');
-        }else{
-            //The local implies local strategy i.e. using username and password for aurthentication.
-            passport.authenticate('local')(req, res, function(){  
-                res.redirect('/secrets');
-            })
-        }
-    })
+app.get("/login", function (req, res) {
+  res.render("login");
+});
 
-})
+app.get("/register", function (req, res) {
+  res.render("register");
+});
 
-app.post('/login', function(req, res){
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
+app.get("/logout", function (req, res) {
+  //callback is made necessary now else only req.logout() was sufficient
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
-    req.login(user, function(err){
-        if(err){
-            console.log(err);
-        }else{
-            passport.authenticate('local')(req, res, function(){
-                res.redirect('/secrets');
-            })
-        }
-    })
+app.post("/register", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-})
+  //Before it was needed to create new user model and then save it. But the modules enable new methods.
+  User.register({ username: username }, password, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      //The local implies local strategy i.e. using username and password for aurthentication.
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
-app.listen(3000, function(){
-    console.log('Server started on port 3000...');
-})
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000...");
+});
